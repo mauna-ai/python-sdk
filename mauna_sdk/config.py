c415d682d60ee3fe8d40703bda518bf8646c9a45
Dmitry Paramonov <asmatic075@gmail.com>
2021-04-06 10:21:46 +0300

    refactor: Update the auth endpoint URL

--- mauna_sdk/config.py
+++ mauna_sdk/config.py
@@ -1,3 +1,2 @@
 API_ENDPOINT = "https://api.mauna.cloud/v1/graphql"
-# TODO: replace with the shorter url
-AUTH_ENDPOINT = "https://us-central1-mauna-ai-290409.cloudfunctions.net/generateHasuraJWT"
+AUTH_ENDPOINT = "https://sdk.mauna.cloud/generateHasuraJWT"
