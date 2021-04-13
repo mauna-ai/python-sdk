614e64a55a5a7488643e3bc6a9d85436ddcb548d
Dmitry Paramonov <asmatic075@gmail.com>
2021-04-13 13:58:44 +0300

    feat: Add an ability to read API and auth endpoints from environment variables

--- mauna_sdk/config.py
+++ mauna_sdk/config.py
@@ -1,2 +1,5 @@
-API_ENDPOINT = "https://api.mauna.cloud/v1/graphql"
-AUTH_ENDPOINT = "https://sdk.mauna.cloud/generateHasuraJWT"
+import os
+
+
+API_ENDPOINT = os.environ.get("MAUNA_API_ENDPOINT", "https://api.mauna.cloud/v1/graphql")
+AUTH_ENDPOINT = os.environ.get("MAUNA_AUTH_ENDPOINT", "https://sdk.mauna.cloud/generateHasuraJWT")
